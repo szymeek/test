e1cc4c5e9c26ab4110ceed653761c17ca70efe7a
szymeek <szymek93@hotmail.com>
2016-10-17 15:45:42 +0200

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/Car.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Car
+    {
+        private double pojemnoscSilnika;
+        private string marka;
+
+        public Car()
+        {
+            this.marka = "Ford";
+            this.pojemnoscSilnika = 2.2;
+        }
+
+        public Car(double pojemnoscSilnika, string marka)
+        {
+            this.marka = marka;
+            this.pojemnoscSilnika = pojemnoscSilnika;
+        }
+
+        public static Car CreateCar()
+        {
+            return new Car();
+        }
+
+        public void PokazMarke()
+        {
+            Console.WriteLine(marka);
+        }
+     
+        static int iloscKol;
+        static Car()
+        {
+            iloscKol = 4;
+        }
+        ~Car()
+        {
+            Console.WriteLine("Zwalnienie pamięci");
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Car samochod1 = new Car();
+            //samochod1.marka = "Ford";
+            Car samochod2 = new Car(1.9, "Seat");
+
+
+            Console.WriteLine(samochod2);
+            Console.ReadKey();
+             */
+
+            Car car1 = new Car();
+            // car1.PokazMarke();
+            Car car2 = new Car(4.8, "Ford");
+
+            Car car3 = Car.CreateCar();
+            Console.WriteLine(car2.PokazMarke);
+            Console.ReadKey();
+
+
+        }
+    }
+}
